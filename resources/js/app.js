c6b927b4ceaea831a9fda479bb56dea956673315
Elif Seyhan <elifseyhan@Elif-MacBook-Air.local>
2022-04-10 16:08:53 +0300

    Prepared app.js, app.vue and addItemForm.vue files. Added fontawesome.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,15 @@ require('./bootstrap');
 
 import { createApp } from 'vue'
 
-import App from './vue/app'
+import App from './vue/app.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+
 
 const app = createApp(App);
-app.mount('#app');
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,35 @@
 <template>
-    <h1>Hello</h1>
-        
-    
+  <div class="todoListContainer">
+    <div class="heading">
+      <h2 id="title">Todo List</h2>
+    </div>
+    <add-item-form />
+  </div>
+  <list-view />
 </template>
 
 <script>
-    export default{
-        
-        }
+import addItemForm from "./addItemForm.vue";
+import listView from "./listView.vue";
+export default {
+  components: {
+    addItemForm,
+    listView,
+  },
+};
 </script>
+<style scoped>
+.todoListContainer {
+  width: 350px;
+  margin: auto;
+}
+
+.heading {
+  background: #e6e6e6;
+  padding: 10px;
+}
+
+#title {
+  text-align: center;
+}
+</style>
